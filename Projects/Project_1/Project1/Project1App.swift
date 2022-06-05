4ed62b5f9c8bfd64d1480ce820dc0aaf7981870d
dateless671 <dreadisback100@gmail.com>
2022-06-05 21:06:22 +0600

    msg

---
+++ Projects/Project_1/Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Yernur Makenov on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
